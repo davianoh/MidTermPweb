44fecd355c48d617b3c842c89eef6567d7c1a908
Davian Benito <71868354+davianoh@users.noreply.github.com>
2022-10-27 12:23:02 +0700

    add project-base-bener

--- ETSPweb/resources/views/emails/testMailJob.blade.php
+++
@@ -1,7 +0,0 @@
-<body>
-    <h1>Hello</h1>
-    {{ $user['name'] }}
-    {{ $user['email'] }}
-    <p>How are you ?</p>
-    <p>Your favorite product {{ $product['name'] }} is Available now.
-    </body>
